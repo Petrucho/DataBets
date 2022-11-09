9d1bcbf68ff2f4e4ae3cb3c20caae0529acd408d
Petrucho <petrucho@post.cz>
2022-11-10 00:21:06 +0300

    success with 6 sites

--- BetCity_WC2022_short.py
+++ BetCity_WC2022_short.py
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 from fake_useragent import UserAgent
 
-import split_div_FonBet
+import split_div_BetCity
 import save_to_csv
 
 user_agent = UserAgent(verify_ssl=False).chrome
@@ -29,7 +29,7 @@ chrome_options.add_argument("user-agent=" + user_agent)
 driver.implicitly_wait(10) # in seconds
 
 # ЧМ 2022
-driver.get("https://www.fon.bet/football-2022/")
+driver.get("https://betcity.ru/ru/line/bets?chmp%5B%5D=167030")
 
 # wait until page will downloaded in browser
 time.sleep(10)
@@ -37,18 +37,19 @@ time.sleep(10)
 # driver.get("https://www.ya.ru")
 try:    
     # page_source = driver.page_source
-    # print(page_source)
+    # print(f'page_source: {page_source}')
 
     # save screenshot of the page
-    driver.save_screenshot('FonBet_world_cup_2022.png')
+    driver.save_screenshot('BetCity_world_cup_2022.png')
 
-    xp_block = '//div[@class="cup__table--6mj4v"]'    
+    xp_block = '//div[@class="line__champ"]'   
     block_name = driver.find_elements(By.XPATH, xp_block)    
     block_name_list = [value.text for value in block_name]
     # print(f'\nblock_name_list: {block_name_list}\n')
 
-    splitted_list = split_div_FonBet.split_list(block_name_list) # split List
-    save_to_csv.save_to_file(splitted_list, 'FonBet') # splitted List saving to csv-file
+    splitted_list = split_div_BetCity.split_list(block_name_list) # split List
+    save_to_csv.save_to_file(splitted_list, 'BetCity') # splitted List saving to csv-file
     
 except:
     print("some error happen !!")
+
--- SportBet_WC2022_short.py
+++ SportBet_WC2022_short.py
@@ -10,7 +10,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 from fake_useragent import UserAgent
 
-import split_div_FonBet
+import split_div_SportBet
 import save_to_csv
 
 user_agent = UserAgent(verify_ssl=False).chrome
@@ -29,26 +29,26 @@ chrome_options.add_argument("user-agent=" + user_agent)
 driver.implicitly_wait(10) # in seconds
 
 # ЧМ 2022
-driver.get("https://www.fon.bet/football-2022/")
+driver.get("https://sportbet.ru/sport?lc=1&lsc=2490&from_left_menu=&ss=all")
 
 # wait until page will downloaded in browser
-time.sleep(10)
+time.sleep(15)
 
-# driver.get("https://www.ya.ru")
 try:    
     # page_source = driver.page_source
-    # print(page_source)
+    # print(f'page_source: {page_source}')
 
     # save screenshot of the page
-    driver.save_screenshot('FonBet_world_cup_2022.png')
+    driver.save_screenshot('SportBet_world_cup_2022.png')
 
-    xp_block = '//div[@class="cup__table--6mj4v"]'    
+    xp_block = '//*[@id="root"]/div[1]/div[4]/div/div[2]/div/div/table'
     block_name = driver.find_elements(By.XPATH, xp_block)    
     block_name_list = [value.text for value in block_name]
     # print(f'\nblock_name_list: {block_name_list}\n')
 
-    splitted_list = split_div_FonBet.split_list(block_name_list) # split List
-    save_to_csv.save_to_file(splitted_list, 'FonBet') # splitted List saving to csv-file
+    splitted_list = split_div_SportBet.split_list(block_name_list) # split List
+    save_to_csv.save_to_file(splitted_list, 'SportBet') # splitted List saving to csv-file
     
 except:
     print("some error happen !!")
+
